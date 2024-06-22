0828882c1770685d7777c5923a7ad7640d099f78
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-06-22 16:27:05 +0530

    parcel bundler

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-{/* <div id="parent">
-      <div id="child">
-        <h1>my name is kaustubh</h1>
-        <h2>i am h2 tag</h2>
-      </div>
-      <div id="child2">
-      <h1>my name is kaustubh</h1>
-      <h2>i am h2 tag</h2>
-      </div>
-      </div> */}
+// {/* <div id="parent">
+//       <div id="child">
+//         <h1>my name is kaustubh</h1>
+//         <h2>i am h2 tag</h2>
+//       </div>
+//       <div id="child2">
+//       <h1>my name is kaustubh</h1>
+//       <h2>i am h2 tag</h2>
+//       </div>
+//       </div> */}
+      import React from "react";
+      import ReactDOM from "react-dom/client";
 const headin=React.createElement(
         "div",
         {id:"parent"},
         [React.createElement("div",{id:"child"},[
                 React.createElement("h1",{},"my name is kaustubh"),
-                React.createElement("h2",{},"i am h2")
+                React.createElement("h2",{},"this is react")
         ]),React.createElement("div",{id:"child2"},[
                 React.createElement("h1",{},"my name is kaustubh"),
-                React.createElement("h2",{},"i am h2")
+                React.createElement("h2",{},"this is react")
         ])])
 
 
@@ -31,3 +33,11 @@ root.render(headin);
 // // console.log(headin); //it is an object
 //         const root=ReactDOM.createRoot(document.getElementById("root"));
 //         root.render(headin);
+
+
+// // in node modules folder parcel have many other dependencies and those may have other that is known as transitive dependencies and that is why the folder size is huge
+// // each dependencies have their own package.json and package-lock.json
+// ^=caret updates smaller changes in latest version
+// ~=tilda upgrades to bigger changes in latest version
+//if you have package.json and package-lock.json you can regenerate node modules
+// npx means executing a package
---
+++ README.md
@@ -0,0 +1,18 @@
+# parcel
+-Dev Build
+-Local Server
+-HMR=Hot Module Replacement
+-File Watching Algorithm - written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-Differnetial Bundling - support older browsers
+-Diagnostics
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
